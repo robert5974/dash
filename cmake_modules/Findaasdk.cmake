23ec022d068ca5caa28d174909b032388e3f27b5
Robert Crowley <robert5974@gmail.com>
2025-12-18 07:27:31 -0500

    Update CMake file

--- cmake_modules/Findaasdk.cmake
+++ cmake_modules/Findaasdk.cmake
@@ -1,45 +1,41 @@
-
-
 find_path(AASDK_INCLUDE_DIR
     aasdk/Version.hpp
-    PATHS ${AASDK_DIR}
-    PATH_SUFFIXES include
+    PATHS ${CMAKE_SOURCE_DIR}/aasdk/include
+    NO_DEFAULT_PATH
 )
 
 find_path(AASDK_PROTO_INCLUDE_DIR
     aasdk_proto/AbsoluteInputEventData.pb.h
-    PATHS ${AASDK_DIR}
+    PATHS ${CMAKE_SOURCE_DIR}/aasdk/build
+    NO_DEFAULT_PATH
 )
 
-find_path(AASDK_LIB_DIR
-    libaasdk.so
-    PATHS ${AASDK_DIR}
-    PATH_SUFFIXES lib
+find_library(AASDK_LIBRARY NAMES aasdk
+    PATHS ${CMAKE_SOURCE_DIR}/aasdk/lib
+    NO_DEFAULT_PATH
+)
+find_library(AASDK_PROTO_LIBRARY NAMES aasdk_proto
+    PATHS ${CMAKE_SOURCE_DIR}/aasdk/lib
+    NO_DEFAULT_PATH
 )
 
-if (AASDK_INCLUDE_DIR AND AASDK_PROTO_INCLUDE_DIR AND AASDK_LIB_DIR)
+if (AASDK_INCLUDE_DIR AND AASDK_PROTO_INCLUDE_DIR AND AASDK_LIBRARY AND AASDK_PROTO_LIBRARY)
     set(AASDK_FOUND TRUE)
 endif()
-  
+
 if (AASDK_FOUND)
     if (NOT aasdk_FIND_QUIETLY)
-        message(STATUS "Found aasdk:")
-        message(STATUS " - Includes: ${AASDK_INCLUDE_DIR}")
-        message(STATUS " - Includes: ${AASDK_PROTO_INCLUDE_DIR}")
-        message(STATUS " - Libraries: ${AASDK_LIB_DIR}")
+        message(STATUS "Found aasdk: ${AASDK_LIBRARY}")
+        message(STATUS "Found aasdk_proto: ${AASDK_PROTO_LIBRARY}")
     endif()
+
     add_library(aasdk INTERFACE)
     target_include_directories(aasdk INTERFACE ${AASDK_INCLUDE_DIR} ${AASDK_PROTO_INCLUDE_DIR})
-    set_target_properties(aasdk PROPERTIES INTERFACE_LINK_DIRECTORIES ${AASDK_LIB_DIR})
-    target_link_libraries(aasdk INTERFACE libaasdk.so libaasdk_proto.so)
+    target_link_libraries(aasdk INTERFACE ${AASDK_LIBRARY} ${AASDK_PROTO_LIBRARY})
 else()
     if (aasdk_FIND_REQUIRED)
-        if(AASDK_INCLUDE_DIR AND NOT AASDK_PROTO_INCLUDE_DIR)
-            message(FATAL_ERROR "aasdk was found but not built.  Perform an in-source build.")
-        else()
-            message(FATAL_ERROR "Could not find aasdk")
-        endif()
+        message(FATAL_ERROR "Could not find aasdk")
     endif()
 endif()
 
-mark_as_advanced(AASDK_INCLUDE_DIRS AASDK_LIBRARIES)
+mark_as_advanced(AASDK_INCLUDE_DIR AASDK_PROTO_INCLUDE_DIR AASDK_LIBRARY AASDK_PROTO_LIBRARY)
