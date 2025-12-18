fd1cf99c0c152565bfc92083d7cb7348e05faf4f
Robert Crowley <robert5974@gmail.com>
2025-12-18 07:58:16 -0500

    Updated finding openauto linker

--- cmake_modules/Findopenauto.cmake
+++ cmake_modules/Findopenauto.cmake
@@ -1,18 +1,19 @@
-set (OPENAUTO_DIR ~/openauto)
+
+set(OPENAUTO_SEARCH_DIR "${CMAKE_SOURCE_DIR}/openauto")
 
 find_path(OPENAUTO_INCLUDE_DIR
-    OpenautoLog.hpp
-    PATHS ${OPENAUTO_DIR}
-    PATH_SUFFIXES include
+    openauto/Service/ServiceFactory.hpp
+    PATHS ${OPENAUTO_SEARCH_DIR}/include
+    NO_DEFAULT_PATH
 )
 
-find_path(OPENAUTO_LIB_DIR
-    libopenauto.so
-    PATHS ${OPENAUTO_DIR}
-    PATH_SUFFIXES lib
+find_library(OPENAUTO_LIBRARY
+    NAMES openauto
+    PATHS ${OPENAUTO_SEARCH_DIR}/lib
+    NO_DEFAULT_PATH
 )
 
-if (OPENAUTO_INCLUDE_DIR AND OPENAUTO_LIB_DIR)
+if (OPENAUTO_INCLUDE_DIR AND OPENAUTO_LIBRARY)
     set(OPENAUTO_FOUND TRUE)
 endif()
   
@@ -20,12 +21,11 @@ if (OPENAUTO_FOUND)
     if (NOT openauto_FIND_QUIETLY)
         message(STATUS "Found openauto:")
         message(STATUS " - Includes: ${OPENAUTO_INCLUDE_DIR}")
-        message(STATUS " - Libraries: ${OPENAUTO_LIB_DIR}")
+        message(STATUS " - Library: ${OPENAUTO_LIBRARY}")
     endif()
     add_library(openauto INTERFACE)
     target_include_directories(openauto INTERFACE ${OPENAUTO_INCLUDE_DIR}) 
-    set_target_properties(openauto PROPERTIES INTERFACE_LINK_DIRECTORIES ${OPENAUTO_LIB_DIR})
-    target_link_libraries(openauto INTERFACE libopenauto.so )
+    target_link_libraries(openauto INTERFACE ${OPENAUTO_LIBRARY})
 else()
     if (openauto_FIND_REQUIRED)
                message(FATAL_ERROR "Could not find openauto")
